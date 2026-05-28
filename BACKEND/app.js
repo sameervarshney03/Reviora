761fb337ad1595766d91a88b3b19652d10b85900
Gaurav Purohit <gauravpurohit685@gmail.com>
2026-05-28 23:13:58 +0530

    feat: changes made in app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,13 +1,24 @@
 // important requires
 const connectDB = require("./src/config/database");
 const express = require("express");
-var cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const authRouter = require("./src/routes/authRouter");
+const notesRouter = require("./src/routes/notesRouter");
+const profileRouter = require("./src/routes/profileRouter");
+const revisionRouter = require("./src/routes/revisionRouter");
 
 const app = express();
 
 // using middleware express.json()
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/", authRouter);
+app.use("/", notesRouter);
+app.use("/", profileRouter);
+app.use("/", revisionRouter);
+
+
 
 // database connection
 connectDB().then(() => {
